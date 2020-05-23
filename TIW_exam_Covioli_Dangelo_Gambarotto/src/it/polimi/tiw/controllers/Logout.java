852bd6a3cadb4a6ef3f26933a75b6a543fc14be8
Alessandro1996 <37804468+Alessandro1996@users.noreply.github.com>
2020-05-23 18:38:42 +0200

    Create Logout servlet
    Created Logout servlet, and updated the "in-session" html files and .properties files with the "logout" field.

---
+++ TIW_exam_Covioli_Dangelo_Gambarotto/src/it/polimi/tiw/controllers/Logout.java
@@ -0,0 +1,50 @@
+package it.polimi.tiw.controllers;
+
+import java.io.IOException;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+/**
+ * Servlet implementation class Logout
+ */
+@WebServlet("/Logout")
+public class Logout extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private TemplateEngine templateEngine;
+
+	public Logout() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public void init() throws ServletException {
+		ServletContext servletContext = getServletContext();
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		this.templateEngine = new TemplateEngine();
+		this.templateEngine.setTemplateResolver(templateResolver);
+//		this.templateEngine.setMessageResolver(new SharedPropertyMessageResolver(servletContext, "i18n", "logout"));
+		templateResolver.setSuffix(".html");
+
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			request.getSession(false).invalidate();
+			response.sendRedirect(getServletContext().getContextPath());
+		} catch (IOException e) {
+			response.sendError(555, "Something wrong in redirecting from logout");
+
+		}
+	}
+
+}
