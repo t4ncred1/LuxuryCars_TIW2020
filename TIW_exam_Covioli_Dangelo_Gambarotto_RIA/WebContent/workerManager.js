5d6dc9d87bf3a9c8d888a8978a44b0b649381431
Tancredi Covioli <tancredi.covioli@gmail.com>
2020-07-01 02:13:23 +0200

    refinished the worker's homepage

--- TIW_exam_Covioli_Dangelo_Gambarotto_RIA/WebContent/workerManager.js
+++ TIW_exam_Covioli_Dangelo_Gambarotto_RIA/WebContent/workerManager.js
@@ -5,12 +5,14 @@
 (function() {
 
 // page components
-  var quotationtable, freetable, pricediv,
+  var quotationtable, freetable, pricediv, success,
+  servererror,
     pageOrchestrator = new PageOrchestrator(); // main controller
 
   window.addEventListener("load", () => {
       pageOrchestrator.start(); // initialize the components
       pageOrchestrator.refresh();
+      pageOrchestrator.show();
   }, false);
 
   function QuotationTable(_div_tabella){ 
@@ -21,11 +23,15 @@
 	  
 	  this.reset = function(){
 		  tabella.style.visibility = "hidden";
+		  tabella.style.display = "none";
 		  error.style.visibility = "hidden";
+		  error.style.display = "none";
+		  body.innerHTML = ""; //empty the body of the table.
+		  _div_tabella.style.display = "none";
 	  };
 
 	  this.show = function(){
-		  var self = this;
+		  var self=this;
 		  makeCall("GET", "ManagedQuotations", null,
 				  function(req) {
 			  if (req.readyState == XMLHttpRequest.DONE) {
@@ -36,27 +42,32 @@
 					  self.update(list);
 					  break;
 				  case 503:
-					  self.error.textContent = message;
-					  self.error.style.visibility = "visible";
+					  error.textContent = message;
+					  error.style.visibility = "visible";
+					  error.style.display = "";
 					  break;
 				  case 411:
 					  
 				  default:
-					  self.error.textContent = 
+					  error.textContent = 
 						  "È avvenuto un errore (Servlet non disponibile)";
-				  	  self.error.style.visibility = "visible";
+				  	  error.style.visibility = "visible";
+				  	  error.style.display = "";
 				  }
 			  }
 		  }
 		  );
+		  _div_tabella.style.display = "";
 	  };
 
 	  this.update = function(quotation_array){
+		  var self=this;
 		  body.innerHTML = ""; //empty the body of the table.
 		  if(quotation_array.length == 0){
-			  self.error.textContent = 
+			  error.textContent = 
 				  "Non è stata fatta ancora alcuna quotazione.";
-		  	  self.error.style.visibility = "visible";
+		  	  error.style.visibility = "visible";
+		  	  error.style.display = "";
 		  }
 		  else{
 			  quotation_array.forEach(item => {
@@ -75,6 +86,7 @@
 				  	row.appendChild(v4cell);
 				  	body.appendChild(row);
 				  	tabella.style.visibility = "visible";
+				  	tabella.style.display = "";
 				  }  	
 			  )
 		  }
@@ -86,14 +98,19 @@
 	  var tabella = _div_tabella.getElementsByTagName("table")[0];
 	  var body = tabella.getElementsByTagName("tbody")[0];
 	  var error = _div_tabella.getElementsByClassName("error")[0];
-
+	  
+	  
 	  this.reset = function(){
 		  tabella.style.visibility = "hidden";
+		  tabella.style.display = "none";
 		  error.style.visibility = "hidden";
+		  error.style.display = "none";
+		  body.innerHTML = ""; //empty the body of the table.
+		  _div_tabella.style.display="none";
 	  };
 	  
 	  this.show = function(){
-		  var self = this;
+		  var self=this;
 		  makeCall("GET", "FreeQuotations", null,
 				  function(req) {
 			  if (req.readyState == XMLHttpRequest.DONE) {
@@ -104,25 +121,29 @@
 					  self.update(list);
 					  break;
 				  case 503:
-					  self.error.textContent = message;
-					  self.error.style.visibility = "visible";
+					  error.textContent = message;
+					  error.style.visibility = "visible";
+					  error.style.display = "";
 					  break;
 				  default:
-					  self.error.textContent = 
+					  error.textContent = 
 						  "È avvenuto un errore (Servlet non disponibile)";
-				  	  self.error.style.visibility = "visible";
+				  	  error.style.visibility = "visible";
+				  	  error.style.display = "";
 				  }
 			  }
 		  }
 		  );
+		  _div_tabella.style.display="";
 	  };
 	  
 	  this.update = function(free_array){
 		  body.innerHTML = ""; //empty the body of the table.
 		  if(free_array.length == 0){
-			  self.error.textContent = 
+			  error.textContent = 
 				  "Non ci sono quotazioni libere.";
-		  	  self.error.style.visibility = "visible";
+		  	  error.style.visibility = "visible";
+		  	  error.style.display = "";
 		  }
 		  else{
 			  free_array.forEach(item => {
@@ -154,51 +175,172 @@
 				  } 
 			  )
 			  tabella.style.visibility = "visible";
+			  tabella.style.display = "";
+			  
 		  }
 	  };
 	  
   }
   
+  function Options(_optionTab){
+	  var optionerror = _optionTab.getElementsByClassName("error")[0];
+	  var optiontable = _optionTab.getElementsByTagName("table")[0];
+	  var optiontablebody = optiontable.getElementsByTagName("tbody")[0];
+	  
+	  this.show = function(item){
+		  self = this;
+		  optiontablebody.innerHTML = ""; //empty the body of the table.
+		  if(item.options.length == 0){
+		  		optiontable.style.display = "none";
+		  		optionerror.textContent="Non sono disponibili opzioni per questo prodotto."
+		  		optionerror.style.visibility = "visible";
+		  }
+		  else{
+			optiontable.style.display = "";
+			optiontable.style.visibility = "visible"
+	  		self.update(item);
+	  	  }
+		  _optionTab.style.display = "";
+		  _optionTab.style.visibility = "visible";
+	  }
+	  this.update = function(item){
+		  item.options.forEach(option =>{
+			var row = document.createElement("tr");
+			var v1cell = document.createElement("td");
+			v1cell.textContent = option.name;
+			row.appendChild(v1cell);
+			var v2cell = document.createElement("td");
+			v2cell.textContent = (option.inOffer ? "si" : "no");
+			row.appendChild(v2cell);
+			optiontablebody.appendChild(row);
+		  });
+	  }
+	  this.reset = function(){
+		  _optionTab.style.display = "none";
+		  optionerror.style.visibility = "hidden";
+	  }
+  
+  }
+  
+  function PriceForm(_div_price){
+	  var inputPrice = document.getElementById("price");
+	  var inputQuotation = document.getElementById("quotation");
+	  var inputButton = document.getElementById("submitprice");
+	  var priceerror = document.getElementById("priceerror");
+	  
+	  this.reset = function(){
+		  _div_price.style.display = "none";
+		  priceerror.style.display = "none";
+		  priceerror.style.visibility = "hidden";
+		  inputQuotation.setAttribute("value","default");
+	  }
+	  
+	  this.show = function(item){
+		  self = this;
+		  _div_price.style.display = "";
+		  _div_price.style.visibility = "visible";
+		  self.update(item);
+	  }
+	  
+	  this.update = function(item){
+		  // I need to substitute the button with a new one
+		  // to remove all the eventListeners 
+		  // Otherwise, new event listeners do not overwrite previous ones.
+		  inputButton_new = inputButton.cloneNode(true);
+		  inputButton.parentNode.replaceChild(inputButton_new, inputButton);
+		  inputButton = inputButton_new;
+		  
+		  inputButton.addEventListener('click', (e) => {
+			  priceerror.style.display = "none";
+			  priceerror.style.visibility = "hidden";
+			  
+			  var price = inputPrice.value;
+			  var form = e.target.closest("form");
+			  if(form.checkValidity()){
+				  if (price <= 0){
+					  var errormessage="Il prezzo inserito deve essere maggiore di 0";
+					  priceerror.textContent = errormessage;
+					  priceerror.style.display = "";
+					  priceerror.style.visibility = "visible";
+				  }
+				  else if (price.toString().indexOf('.') != -1 
+					  && price.toString().split('.')[1].length > 2){
+					  var errormessage = "Si prega di inserire un prezzo valido che rispetti il formato visualizzato";
+					  priceerror.textContent = errormessage;
+					  priceerror.style.display = "";
+					  priceerror.style.visibility = "visible";
+				  }
+				  
+				  else{
+					  inputQuotation.setAttribute("value", item.quotationId);
+					  makeCall("POST", 'PricePost', e.target.closest("form"),
+			            function(req) {
+			              if (req.readyState == XMLHttpRequest.DONE) {
+			                
+			            	var message = req.responseText; // error message or message response.
+			                
+			                if (req.status == 200) {
+			                	pageOrchestrator.refresh();
+			                    pageOrchestrator.show();
+			                    success.textContent = message;
+			                    success.style.display = "";
+			                    success.style.visibility = "visible"
+			                } 
+			                else if (req.status == 503){
+			                	pageOrchestrator.refresh();
+			                    pageOrchestrator.show();
+			                	servererror.textContent = message;
+			                	servererror.style.display = "";
+			                	servererror.style.visibility = "visible"
+			                }
+			                else{
+			                  priceerror.textContent = message;
+			  				  priceerror.style.display = "";
+			  				  priceerror.style.visibility = "visible";
+			                }
+			              }
+			            }
+			          ,true);
+				  }
+			  }
+			  else{
+				  form.reportValidity();
+			  }
+		  })
+	  }
+  }
+  
   function PriceQuotation(_div_info){
+	  
+	  var optionTab = document.getElementById("optiontable");
+	  var optionsTable = new Options(optionTab);
+	  
+	  var formDiv = document.getElementById("priceinsertion");
+	  var priceForm = new PriceForm(formDiv);
+	  
+	  var infoTab = document.getElementById("infotable");
 	  
 	  var productEntry = document.getElementById("productname");
 	  var clientEntry = document.getElementById("clientname");
-	  
-	  
-	  var optionTable = document.getElementById("optiontable");
-	  var optionerror = optionTable.getElementsByClassName("error")[0];
-	  var optionTableBody = optionTable.getElementsByTagName("tbody")[0];
-	  
 	  this.reset = function(){
-		  //TODO
+		  _div_info.style.display = "none";
+		  infoTab.style.visibility = "hidden";
+		  optionsTable.reset();
+		  priceForm.reset();
 	  };
 	  
 	  this.show = function(item){
-	  	
-		//Option table
-		clientEntry.textContent = item.clientUsername;
-	  	productEntry.textContent = item.productName;
-	  	document.getElementById("infotable").style.visibility = "visible";
-	  	
-	  	optionTableBody.innerHTML = ""; //empty the body of the table.
-	  	if(item.options.length == 0){
-	  		optionerror.textContent="Non sono disponibili opzioni per questo prodotto."
-	  		optionerror.style.visibility = "visible";
-	  	}
-	  	else{
-	  		item.options.forEach(option =>{
-		  		var row = document.createElement("tr");
-			  	var v1cell = document.createElement("td");
-			  	v1cell.textContent = option.name;
-			  	row.appendChild(v1cell);
-			  	var v2cell = document.createElement("td");
-			  	v2cell.textContent = (option.inOffer ? "si" : "no");
-			  	row.appendChild(v2cell);
-			  	optionTableBody.appendChild(row);
-		  	});
-		  	optionTable.style.visibility = "visible";
-	  	}
-	  	//TODO
+		  this.update(item);
+		  optionsTable.show(item);
+		  priceForm.show(item);
+		  infoTab.style.visibility = "visible";
+		  _div_info.style.display= "";
+		  _div_info.style.visibility= "visible";
+	  }
+	  
+	  this.update = function(item){
+		  clientEntry.textContent = item.clientUsername;
+		  productEntry.textContent = item.productName;
 	  }
   }
 
@@ -211,15 +353,27 @@
 		
 	    var quotTab = document.getElementById("quotationtable");
 	    quotationtable = new QuotationTable(quotTab);
-	    quotationtable.show();
 	    
 	    var freeTab = document.getElementById("freetable");
 	    freetable = new FreeTable(freeTab);
-	    freetable.show();
+	    
+	    success = document.getElementById("success");
+	    servererror = document.getElementById("servererror");
 	};
 	
+	this.show = function(){
+		quotationtable.show();
+		freetable.show();
+	}
+	
 	this.refresh = function(){
-		//TODO insert here which elements shall be hidden/reset.
+		quotationtable.reset();
+		freetable.reset();
+		pricediv.reset();
+		success.style.display = "none";
+		success.textContent = "Il prezzo è stato inserito correttamente.";
+		servererror.style.display = "none";
+		servererror.textContent = "È stato riscontrato un errore lato server.";
 	};
 
   }
---
+++ TIW_exam_Covioli_Dangelo_Gambarotto_RIA/src/it/polimi/tiw/controllers/PricePost.java
@@ -0,0 +1,152 @@
+package it.polimi.tiw.controllers;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.UnavailableException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import it.polimi.tiw.DAO.QuotationDAO;
+import it.polimi.tiw.beans.UserBean;
+
+/**
+ * Servlet implementation class PricePost
+ */
+
+@WebServlet("/PricePost")
+@MultipartConfig
+public class PricePost extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private Connection connection = null;
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PricePost() {
+        super();
+    }
+    
+    public void init() throws ServletException {
+		try {
+			ServletContext context = getServletContext();
+			String driver = context.getInitParameter("dbDriver");
+			String url = context.getInitParameter("dbUrl");
+			String user = context.getInitParameter("dbUser");
+			String password = context.getInitParameter("dbPassword");
+			Class.forName(driver);
+			connection = DriverManager.getConnection(url, user, password);
+
+		} catch (ClassNotFoundException e) {
+			throw new UnavailableException("Can't load database driver");
+		} catch (SQLException e) {
+			throw new UnavailableException("Couldn't get db connection");
+		}
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/plain;charset=UTF-8");	
+		String language="_it";
+		
+		HttpSession s = request.getSession(false);
+		UserBean u = (UserBean) s.getAttribute("user");
+		Double price = 0.0;
+		int intPrice = 0;
+		
+		QuotationDAO qDAO = new QuotationDAO(connection);
+		int qID = 0;
+		
+		//check if parameter quotation is correct
+		try {
+			if( request.getParameter("quotation")!= null)
+				qID = Integer.parseInt(request.getParameter("quotation"));
+			else {
+				System.out.println("id è nullo.");
+				throw new NullPointerException();
+			}
+		} catch(NumberFormatException|NullPointerException e) {
+			String errormessage = "L'id passato non è un valore valido";
+			response.setStatus(422);
+			response.getWriter().write(errormessage);
+			return;
+		}
+		
+		//check if parameter price is correct
+		try {
+			if( request.getParameter("price")!= null)
+				price = Double.parseDouble(request.getParameter("price"));
+			else throw new NullPointerException();
+		} catch(NumberFormatException|NullPointerException e) {
+			String errormessage = "Si prega di inserire un prezzo valido che rispetti il formato visualizzato";
+			response.setStatus(422);
+			response.getWriter().write(errormessage);
+			return;
+		}
+
+		//check if the quotation ID is set
+		try {
+			List<Integer> freeIDs = qDAO.getFreeQuotations(language).stream()
+					.map(id -> id.getQuotationId())
+					.collect(Collectors.toList());
+			if(!freeIDs.contains(qID)) {
+				String errormessage = "L'id passato non è tra gli ID liberi";
+				response.setStatus(422);
+				response.getWriter().write(errormessage);
+				return;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			String errormessage = "Il database non funziona";
+			response.setStatus(HttpServletResponse.SC_SERVICE_UNAVAILABLE);
+			response.getWriter().write(errormessage);
+			return;
+		}
+		
+		if(price<=0) {
+			String errormessage = "Il prezzo inserito deve essere maggiore di 0";
+			response.setStatus(422);
+			response.getWriter().write(errormessage);
+			return;
+		}
+		//check if the price float is in a correct format
+		if(BigDecimal.valueOf(price).scale() > 2) {
+			//redirect to the page, but with the additional parameter "error" set to true.
+			String errormessage = "Si prega di inserire un prezzo valido che rispetti il formato visualizzato";
+			response.setStatus(422);
+			response.getWriter().write(errormessage);
+			return;
+		}
+		Double newprice = price * 100;
+		intPrice = newprice.intValue();
+				
+		//set the price for the quotation
+		try {
+			qDAO.setQuotationPrice(qID, intPrice, u.getUserid());
+		} catch (SQLException e) {
+			e.printStackTrace();
+			String errormessage = "Il database non funziona";
+			response.setStatus(HttpServletResponse.SC_SERVICE_UNAVAILABLE);
+			response.getWriter().write(errormessage);
+			return;
+		}
+		response.setStatus(HttpServletResponse.SC_OK);
+		response.getWriter().write("Il nuovo prezzo è stato inserito correttamente!");
+		return;
+	}
+}
