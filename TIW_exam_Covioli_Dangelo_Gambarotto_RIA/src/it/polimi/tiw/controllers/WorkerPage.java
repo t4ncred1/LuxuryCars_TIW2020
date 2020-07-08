77a84bf0b185c4d16024d387cbbbdd723ef5bd15
luca gambarotto <luca.gambarotto@mail.polimi.it>
2020-07-08 08:09:05 +0200

    Substituted JSP mapping with dispatcher servlet

---
+++ TIW_exam_Covioli_Dangelo_Gambarotto_RIA/src/it/polimi/tiw/controllers/WorkerPage.java
@@ -0,0 +1,46 @@
+/*  _______ _______          __                                    
+ * |__   __|_   _\ \        / /                                    
+ *    | |    | |  \ \  /\  / /                                     
+ *    | |    | |   \ \/  \/ /                                      
+ *    | |   _| |_   \  /\  /                                       
+ *    |_|  |_____|   \/  \/   
+ * 
+ * exam project - a.y. 2019-2020
+ * Politecnico di Milano
+ * 
+ * Tancredi Covioli   mat. 944834
+ * Alessandro Dangelo mat. 945149
+ * Luca Gambarotto    mat. 928094
+ */
+
+package it.polimi.tiw.controllers;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/Worker")
+public class WorkerPage extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public WorkerPage() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher("/homeworker.html");
+		rd.forward(request, response);
+		return;
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
